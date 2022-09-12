35a009bfb6bb5e0b7d86462f56e6f542c1976407
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-09-12 10:55:10 -0700

    final navigationTitle fixes

--- Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Views/RecipesListView.swift
@@ -14,14 +14,14 @@ struct RecipesListView: View {
     let listTextColor = AppColor.foreground
     
     var body: some View {
-            List {
-                ForEach(recipes) { recipe in
-                    NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
-                }
-                .listRowBackground(listBackgroundColor)
-                .foregroundColor(listTextColor)
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
             }
-            .navigationTitle(navigationTitle)
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
+        }
+        .navigationTitle(navigationTitle)
     }
 }
 
