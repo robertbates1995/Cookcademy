4c952b8c80fa781f69c661338344ac870dd6f0ba
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-09-11 18:25:01 -0700

    implemented direction list and padding

--- Cookcademy/CookcademyApp.swift
+++ Cookcademy/CookcademyApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 @main
+
 struct CookcademyApp: App {
     var body: some Scene {
         WindowGroup {
--- Cookcademy/Views/RecipeDetailView.swift
+++ Cookcademy/Views/RecipeDetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     let recipe: Recipe
-
+ 
     var body: some View {
         VStack{
             HStack{
@@ -26,23 +26,33 @@ struct RecipeDetailView: View {
             }
             List{
                 Section(header: Text("Ingredients")) {
-                    ForEach(recipe.ingredients.indices, id: \.self) { index in
+                    ForEach(recipe.ingredients.indices, id: \.self){ index in
                         let ingredient = recipe.ingredients[index]
                         Text(ingredient.description)
-                        
                     }
                 }
             }
+            .padding(7)
+            List{
+                Section(header: Text("Directions")) {
+                    ForEach(recipe.directions.indices, id: \.self){ index in
+                        let direction = recipe.directions[index]
+                        Text(direction.description)
+                    }
+                }
+            }
+            .padding(7)
         }
         .navigationTitle(recipe.mainInformation.name)
     }
 }
 
 struct RecipeDetailView_Previews: PreviewProvider {
-    @State static var recipe = Recipe.testRecipes[0]
+    @State static var recipe = Recipe.testRecipes[1]
     static var previews: some View {
         NavigationView{
             RecipeDetailView(recipe: recipe)
         }
     }
 }
+
