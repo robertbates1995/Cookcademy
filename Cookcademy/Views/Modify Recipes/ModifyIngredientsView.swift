9e3d0d249d5c48c9e8e596ad8cd74fd53b3d28ce
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-10 11:07:29 -0700

    started ingredient ui and displayed it from parent

---
+++ Cookcademy/Views/Modify Recipes/ModifyIngredientsView.swift
@@ -0,0 +1,38 @@
+//
+//  ModifyIngredientsView.swift
+//  Cookcademy
+//
+//  Created by Robert Bates on 10/10/22.
+//
+
+import SwiftUI
+
+struct ModifyIngredientsView: View {
+    @Binding var ingredients: [Ingredient]
+    @State private var newIngredient = Ingredient(name: "", quantity: 0.0, unit: .none)
+    
+    var body: some View {
+        VStack {
+            if ingredients.isEmpty {
+                Spacer()
+                NavigationLink("Add the first ingredient", destination: ModifyIngredientView(ingredient: newIngredient))
+                Spacer()
+            } else {
+                List{
+                    ForEach(ingredients.indices, id: \.self) { index in
+                        let ingredient = ingredients[index]
+                        Text(ingredient.description)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ModifyIngredientsView_Previews: PreviewProvider {
+    @State static var emptyIngredients = [Ingredient]()
+    
+    static var previews: some View {
+        ModifyIngredientsView(ingredients: $emptyIngredients)
+    }
+}
