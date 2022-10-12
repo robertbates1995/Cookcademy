2784814af9ce017434af6fcf9f41dfa2f6258104
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-12 15:40:43 -0700

    further implementing #7

--- Cookcademy/Views/Modify Recipes/ModifyDirectionView.swift
+++ Cookcademy/Views/Modify Recipes/ModifyDirectionView.swift
@@ -10,13 +10,29 @@ import SwiftUI
 struct ModifyDirectionView: View {
     
     @Binding var direction: Direction
-    
     let createAction: (Direction) -> Void
+    
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
     
+    @Environment(\.presentationMode) private var mode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Direction Description", text: $direction.description)
+                .listRowBackground(listBackgroundColor)
+            Toggle("Optional", isOn: $direction.isOptional)
+                .listRowBackground(listBackgroundColor)
+            HStack {
+                Spacer()
+                Button("Save") {
+                    createAction(direction)
+                    mode.wrappedValue.dismiss()
+                }
+                Spacer()
+            }.listRowBackground(listBackgroundColor)
+        }
+        .foregroundColor(listTextColor)
     }
 }
 
