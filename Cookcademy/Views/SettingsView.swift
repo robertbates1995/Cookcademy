64cd99114a72192184aece5c508bc1e02264f5c2
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-21 16:23:36 -0700

    adding settings page for #11

---
+++ Cookcademy/Views/SettingsView.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsView.swift
+//  Cookcademy
+//
+//  Created by Robert Bates on 10/21/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  @State private var hideOptionalSteps: Bool = false
+  @State private var listBackgroundColor = AppColor.background
+  @State private var listTextColor = AppColor.foreground
+ 
+  var body: some View {
+    NavigationView {
+      Form {
+        ColorPicker("List BackgroundColor", selection: $listBackgroundColor)
+          .padding()
+          .listRowBackground(listBackgroundColor)
+        ColorPicker("Text Color", selection: $listTextColor)
+          .padding()
+          .listRowBackground(listBackgroundColor)
+        Toggle("Hide Optional Steps", isOn: $hideOptionalSteps)
+          .padding()
+          .listRowBackground(listBackgroundColor)
+      }
+      .foregroundColor(listTextColor)
+      .navigationTitle("Settings")
+    }
+  }
+}
