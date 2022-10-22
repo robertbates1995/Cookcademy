e100f9dea7ac1870ce832964b5dacf024f971688
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-21 17:11:29 -0700

    conformed to AppStorage to allow persistence  #11

--- Cookcademy/Views/SettingsView.swift
+++ Cookcademy/Views/SettingsView.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 
 struct SettingsView: View {
-  @State private var hideOptionalSteps: Bool = false
-  @State private var listBackgroundColor = AppColor.background
-  @State private var listTextColor = AppColor.foreground
- 
-  var body: some View {
-    NavigationView {
-      Form {
-        ColorPicker("List BackgroundColor", selection: $listBackgroundColor)
-          .padding()
-          .listRowBackground(listBackgroundColor)
-        ColorPicker("Text Color", selection: $listTextColor)
-          .padding()
-          .listRowBackground(listBackgroundColor)
-        Toggle("Hide Optional Steps", isOn: $hideOptionalSteps)
-          .padding()
-          .listRowBackground(listBackgroundColor)
-      }
-      .foregroundColor(listTextColor)
-      .navigationTitle("Settings")
+    @State private var hideOptionalSteps: Bool = false
+    @AppStorage("listBackgroundColor") private var listBackgroundColor = AppColor.background
+    @AppStorage("listTextColor") private var listTextColor = AppColor.foreground
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                ColorPicker("List BackgroundColor", selection: $listBackgroundColor)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+                ColorPicker("Text Color", selection: $listTextColor)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+                Toggle("Hide Optional Steps", isOn: $hideOptionalSteps)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+            }
+            .foregroundColor(listTextColor)
+            .navigationTitle("Settings")
+        }
     }
-  }
 }
