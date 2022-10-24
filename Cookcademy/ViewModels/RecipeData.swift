f34cca5fc0e95af76de403d2f0c29ef6447f3242
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-10-24 14:37:09 -0700

    Started Framework for Saving Recipes

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -17,12 +17,12 @@ class RecipeData: ObservableObject {
     func recipes(for category: MainInformation.Category) -> [Recipe] {
         var filteredRecipes = [Recipe]()
         for recipe in recipes {
-          if recipe.mainInformation.category == category {
-            filteredRecipes.append(recipe)
-          }
+            if recipe.mainInformation.category == category {
+                filteredRecipes.append(recipe)
+            }
         }
         return filteredRecipes
-      }
+    }
     
     func index(of recipe: Recipe) -> Int? {
         for i in recipes.indices {
@@ -31,5 +31,43 @@ class RecipeData: ObservableObject {
             }
         }
         return nil
+    }
+    
+    func saveRecipes() {
+        do {
+            let encodedData = try JSONEncoder().encode(recipes)
+            try encodedData.write(to: recipesFileURL)
+        }
+        catch {
+            fatalError("An error occurred while saving recipes: \(error)")
+        }
+    }
+    
+    func loadRecipes() {
+        guard let data = try? Data(contentsOf: recipesFileURL) else { return }
+        do {
+            let savedRecipes = try JSONDecoder().decode([Recipe].self, from: data)
+            recipes = savedRecipes
+        }
+        catch {
+            fatalError("An error occurred while saving recipes: \(error)")
+        }
+    }
+    
+    func add(recipe: Recipe) {
+        if recipe.isValid {
+            recipes.append(recipe)
+            saveRecipes()
+        }
+    }
+    
+    private var recipesFileURL: URL {
+        do {
+            let documentsDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            return documentsDirectory.appendingPathComponent("recipeData")
+        }
+        catch {
+            fatalError("An error occurred while getting the url: \(error)")
+        }
     }
 }
